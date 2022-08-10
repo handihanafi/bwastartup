b8d3703fde0a2fa9bb928d6ac6c22fdac68c6ee1
handihanafi <hanafihandi96@gmail.com>
2022-08-10 10:14:58 +0700

    auth middleware

--- handler/user.go
+++ handler/user.go
@@ -133,7 +133,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	userID := 1
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
