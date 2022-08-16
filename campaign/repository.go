b927ad914112e179a18168005ca50c4fac257a31
handihanafi <hanafihandi96@gmail.com>
2022-08-16 15:58:08 +0700

    updated campaign endpoint

--- campaign/repository.go
+++ campaign/repository.go
@@ -7,6 +7,7 @@ type Repository interface {
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
+	Update(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -55,5 +56,13 @@ func (r *repository) Save(campaign Campaign) (Campaign, error) {
 		return campaign, err
 	}
 
+	return campaign, nil
+}
+
+func (r *repository) Update(campaign Campaign) (Campaign, error) {
+	err := r.db.Save(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
 	return campaign, nil
 }
